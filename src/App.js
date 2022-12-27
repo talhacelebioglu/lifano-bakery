02b664420d53e72e911bfbbf6ac890e321aec4b6
talhacelebioglu <104030358+talhacelebioglu@users.noreply.github.com>
2022-12-27 18:13:12 +0300

    add snowfall

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Snowfall from "react-snowfall";
 import CategoryList from "./components/CategoryList";
 import Footer from "./components/Footer";
 import Landing from "./components/Landing";
@@ -8,6 +9,12 @@ function App() {
   return (
     <>
       <div className="App container mx-auto">
+        <Snowfall
+          snowflakeCount={70}
+          radius={[0.5, 3]}
+          speed={[0.5, 3.5]}
+          wind={[-0.5, 2]}
+        />
         <div className="px-5">
           <Landing />
           <Navbar />
